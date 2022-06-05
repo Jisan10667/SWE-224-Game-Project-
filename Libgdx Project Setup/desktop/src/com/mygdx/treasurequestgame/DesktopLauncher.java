2fe435f2ec214c4d4823e500c71b28db368437e4
Jisan Ahmed <65423479+Jisan10667@users.noreply.github.com>
2022-06-05 22:23:52 +0600

    Add files via upload

---
+++ Libgdx Project Setup/desktop/src/com/mygdx/treasurequestgame/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.mygdx.treasurequestgame;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.mygdx.treasurequestgame.Treasure Hunt;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("Treasure Hunt");
+		new Lwjgl3Application(new Treasure Hunt(), config);
+	}
+}
