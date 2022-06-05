d7c55e3a12b11197f1d3448f908d37f0b0165868
Jisan Ahmed <65423479+Jisan10667@users.noreply.github.com>
2022-06-05 23:04:30 +0600

    Add files via upload

---
+++ Final/desktop/src/main/java/TreasureQuestGame2/desktop/DesktopLauncher.java
@@ -0,0 +1,14 @@
+package TreasureQuestGame2.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import TreasureQuestGame2.TreasureHunt;
+
+/** Launches the desktop (LWJGL) application. */
+public class DesktopLauncher
+{
+    public static void main (String[] args)
+    {
+        TreasureHunt myGame = new TreasureHunt();
+        LwjglApplication launcher = new LwjglApplication( myGame, "Treasure Hunt", 800, 600 );
+    }
+}
